090066cd48485306a641ad2d0d064a69d7b16c08
champ7 <user7@mail.ru>
2024-04-12 11:44:56 +0200

    Добавьте файлы проекта.

---
+++ Solodkaya_lab6.7.8.9/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Solodkaya_lab6._7._8._9
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
